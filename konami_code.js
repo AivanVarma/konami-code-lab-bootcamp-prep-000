c6d56122ca02e8d2b4e9a09410ede0424d930cf3
Eero Saarinen <eiusaarinen@gmail.com>
2018-03-26 08:15:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,24 @@ function init() {
 
   const body = document.body
   
+  let index = 0;
+ 
+  // This is the function that would be invoked by the event listener.
+  function onKeyDownHandler(e) {
+   const key = parseInt(e.detail || e.which);
+ 
+    if (key === alphabet[index]) {
+     index++;
   
+      if (index === alphabet.length) {
+       alert("Hurray!");
+ 
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
   
   body.eventListener('keydown')
 }
